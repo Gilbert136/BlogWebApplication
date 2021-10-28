f39a470d50eca000b69d14273f64397bdce1a156
gilbert <gilbertkwameadu@gmail.com>
2021-10-28 11:59:18 +0000

    commit changes updates

--- BlogWebApplication.Data/ApplicationDbContext.cs
+++ BlogWebApplication.Data/ApplicationDbContext.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+﻿using BlogWebApplication.Data.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
 
